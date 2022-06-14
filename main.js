9d440fbb38a720c51ebdfd07361a3d975ad5ac27
alessandrolongo99 <longoalessandro153@gmail.com>
2022-06-14 13:42:37 +0200

    fatto

---
+++ main.js
@@ -0,0 +1,18 @@
+const km = prompt('Quanti chilometri vuoi percorrere?');
+const age = prompt('Quanti anni hai?');
+const tax = 0.21;
+
+if (isNaN(km) || isNaN(age)) {
+    console.log('Non hai inserito un numero ricarica la pagina');
+} else {
+    let price = km * tax;
+    if (age < 18) {
+        price = price - ((price / 100) * 20);
+        console.log(`il prezzo e' di ${price.toFixed(2)} euro`);
+    } else if (age > 65) {
+        price = price - ((price / 100) * 40);
+        console.log(`il prezzo e' di ${price.toFixed(2)} euro`);
+    } else {
+        console.log(`il prezzo e' di ${price.toFixed(2)} euro`);
+    }
+}
